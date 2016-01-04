01bd280859c74d8228fbd7becb901f42804e9862
thiba <thibaultroy@live.fr>
2016-01-04 14:56:18 +0100

    commit nouveau groupe

---
+++ app/src/main/java/fluidpirates/fluidpirates_android/New_group.java
@@ -0,0 +1,28 @@
+package fluidpirates.fluidpirates_android;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class New_group extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_group);
+        TextView top_bar_text = (TextView) findViewById(R.id.top_bar_text);
+        top_bar_text.setText("Groupes");
+        final Button create_group = (Button) findViewById(R.id.create_group);
+        create_group.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(New_group.this, Groups.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
