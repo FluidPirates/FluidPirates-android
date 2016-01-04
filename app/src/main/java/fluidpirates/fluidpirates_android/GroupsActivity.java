1befe88716c0b7906b448529d37dd52bc23bb701
Dorian <dorian@doma.io>
2016-01-04 19:30:01 +0100

    post requests work, need to store token and transition to other views

--- app/src/main/java/fluidpirates/fluidpirates_android/GroupsActivity.java
+++ app/src/main/java/fluidpirates/fluidpirates_android/GroupsActivity.java
@@ -29,58 +29,21 @@ import utils.GetJsonArrayAsync;
 
 
 public class GroupsActivity extends Activity {
-    public String[] name_group;
-    public String[] nb_members;
-
     private static final String GROUPS_URL = "http://fluidpirates.com/api/groups?token=1";
-
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.listViewGestion();
+
         TextView top_bar_text = (TextView) findViewById(R.id.top_bar_text);
         top_bar_text.setText("Groupes");
+
         NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
         TextView tv = (TextView) navView.inflateHeaderView(R.layout.nav_header_main).findViewById(R.id.nom_tv);
         tv.setText("Nom session");
 
         loadFromAPI(GROUPS_URL);
-    }
 
-    private void loadFromAPI(String url) {
-        (new FetchGroupsIndex(this)).execute(url);
-    }
-
-    protected void listViewGestion() {
-        ListView lv;
-        name_group = this.getResources().getStringArray(R.array.name_group);
-        nb_members = this.getResources().getStringArray(R.array.nb_members);
-        final ArrayList<Group_class> listGroup = new ArrayList<Group_class>();
-        int i;
-        for (i = 0; i < name_group.length; i++) {
-            listGroup.add(new Group_class(name_group[i], nb_members[i]));
-        }
-        Group_adapter adapter = new Group_adapter(this, listGroup);
-        setContentView(R.layout.activity_groups);
-        lv = ((ListView) this.findViewById(R.id.listGroups));
-        lv.setAdapter(adapter);
-
-        lv.setOnItemClickListener(new OnItemClickListener() {
-
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(GroupsActivity.this, CurrentGroupActivity.class);
-                intent.putExtra("group", listGroup.get(position).nom);
-                startActivity(intent);
-            }
-        });
         final Button createButton = (Button) findViewById(R.id.create_group);
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -91,72 +54,8 @@ public class GroupsActivity extends Activity {
         });
     }
 
-
-    class Group_class {
-
-        public String nom;
-        public String nb_members;
-
-        public Group_class(String aNom, String anb_membres) {
-            nom = aNom;
-            nb_members = anb_membres;
-        }
-    }
-
-    class Group_adapter extends BaseAdapter {
-
-        // Une liste de personnes
-        private List<Group_class> mListG;
-
-        //Le contexte dans lequel est présent notre adapter
-        private Context mContext;
-
-        //Un mécanisme pour gérer l'affichage graphique depuis un layout XML
-        private LayoutInflater mInflater;
-
-        public Group_adapter(Context context, List<Group_class> aListG) {
-            mContext = context;
-            mListG = aListG;
-            mInflater = LayoutInflater.from(mContext);
-        }
-
-        public int getCount() {
-            return mListG.size();
-        }
-
-        public Object getItem(int position) {
-            return mListG.get(position);
-        }
-
-        public long getItemId(int position) {
-            return position;
-        }
-
-
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LinearLayout layoutItem;
-
-            //(1) : Réutilisation des layouts
-            if (convertView == null) {
-                //Initialisation de notre item à partir du  layout XML "personne_layout.xml"
-                layoutItem = (LinearLayout) mInflater.inflate(R.layout.fragment_group_element, parent, false);
-
-            } else {
-                layoutItem = (LinearLayout) convertView;
-            }
-
-            //(2) : Récupération des TextView de notre layout
-            TextView tv_Nom = (TextView) layoutItem.findViewById(R.id.group_nom);
-            TextView tv_members = (TextView) layoutItem.findViewById(R.id.group_nombre_membres);
-
-
-            //(3) : Renseignement des valeurs
-            tv_Nom.setText(mListG.get(position).nom);
-            tv_members.setText(mListG.get(position).nb_members);
-
-            //On retourne l'item créé.
-            return layoutItem;
-        }
+    private void loadFromAPI(String url) {
+        (new FetchGroupsIndex(this)).execute(url);
     }
 
     private class FetchGroupsIndex extends GetJsonArrayAsync {
@@ -180,7 +79,7 @@ public class GroupsActivity extends Activity {
                             jsonObject.getString("domain")));
                 }
 
-                ListView listView = (ListView) findViewById (R.id.listGroups);
+                ListView listView = (ListView) findViewById(R.id.listGroups);
                 if (listView != null) {
                     listView.setAdapter(new GroupAdapter(getApplicationContext(), R.layout.group_list_item, objects));
                 }
--- app/src/main/java/utils/JsonHelper.java
+++ app/src/main/java/utils/JsonHelper.java
@@ -1,105 +1,92 @@
 package utils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import android.content.Context;
+import android.nfc.Tag;
+import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.content.Context;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import models.Params;
 
 public class JsonHelper {
-	public static JSONArray getJsonArrayFromResource(Context context, int resourceId) throws JSONException, IOException {
-		InputStream is = context.getResources().openRawResource(resourceId);
-		return new JSONArray(getStringFromInputStream(is));
-	}
-	
-	public static JSONObject getJsonObjectFromResource(Context context, int resourceId) throws JSONException, IOException {
-		InputStream is = context.getResources().openRawResource(resourceId);
-		return new JSONObject(getStringFromInputStream(is));
-	}
-	
-	public static JSONArray getJsonArrayFromUrl(String url) throws MalformedURLException, JSONException, IOException
-	{
-		return getJsonArrayFromUrl(url, 0, 0);
-	}
-	
-	public static JSONArray getJsonArrayFromUrl(String url, int connectTimeout, int readTimeout) throws MalformedURLException, JSONException, IOException {
-		return new JSONArray(getStringFromUrl(url, connectTimeout, readTimeout));
-	}
-	
-	public static JSONObject getJsonObjectFromUrl(String url) throws MalformedURLException, JSONException, IOException 
-	{
-		return getJsonObjectFromUrl(url, 0, 0);
-	}
-	
-	public static JSONObject getJsonObjectFromUrl(String url, int connectTimeout, int readTimeout) throws MalformedURLException, JSONException, IOException 
-	{
-		return new JSONObject(getStringFromUrl(url, connectTimeout, readTimeout));
-	}
+    static final private String TAG = "JsonHelper";
 
-	public static JSONObject postJsonObjectFromUrl(String url, Params params) throws IOException {
-		URL urlObject = new URL(url);
+    public static JSONArray getJsonArrayFromResource(Context context, int resourceId) throws JSONException, IOException {
+        InputStream is = context.getResources().openRawResource(resourceId);
+        return new JSONArray(getStringFromInputStream(is));
+    }
 
-		HttpURLConnection conn = (HttpURLConnection) urlObject.openConnection();
-		conn.setDoOutput( true );
-		conn.setRequestMethod( "POST" );
-		int responseCode = conn.getResponseCode();
+    public static JSONObject getJsonObjectFromResource(Context context, int resourceId) throws JSONException, IOException {
+        InputStream is = context.getResources().openRawResource(resourceId);
+        return new JSONObject(getStringFromInputStream(is));
+    }
 
-		if (responseCode != 200) {
-			throw new IOException("The HTTP response code was " + Integer.toString(responseCode));
-		}
+    public static JSONArray getJsonArrayFromUrl(String url) throws MalformedURLException, JSONException, IOException {
+        return getJsonArrayFromUrl(url);
+    }
 
-		String jsonString  = "";
+    public static JSONObject getJsonObjectFromUrl(String url) throws MalformedURLException, JSONException, IOException {
+        return getJsonObjectFromUrl(url);
+    }
 
-		try {
-			jsonString = getStringFromInputStream(conn.getInputStream());
-		} finally {
-			conn.disconnect();
-		}
-		return new JSONObject();
-	}
-	
-	static private String getStringFromInputStream(InputStream is) throws IOException {	
-		BufferedReader br = new BufferedReader(new InputStreamReader(is));
-		StringBuilder sb = new StringBuilder();
-		String line = "";
-		while((line = br.readLine()) != null) {
-			sb.append(line);
-		}
-		return sb.toString();
-	}
-	
-	private static String getStringFromUrl(String url, int connectTimeout, int readTimeout) throws MalformedURLException, JSONException, IOException {
-		URL urlObject = new URL(url);
-		HttpURLConnection urlConn = (HttpURLConnection) urlObject.openConnection();
-		int responseCode = urlConn.getResponseCode();
-		if (responseCode != 200) {
-			throw new IOException("The HTTP response code was " + Integer.toString(responseCode));
-		}
+    public static JSONObject postJsonObjectFromUrl(String url, Params params) throws IOException {
+        URL urlObject = new URL(url);
 
-		String jsonString  = "";
-		
-		if (connectTimeout != 0) {
-			urlConn.setConnectTimeout(connectTimeout);
-		}
-		if (readTimeout != 0) {
-			urlConn.setReadTimeout(readTimeout);
-		}
-		
-		try {
-			jsonString = getStringFromInputStream(urlConn.getInputStream());
-		} finally {
-			urlConn.disconnect();
-		}
-		return jsonString;
-	}
+        HttpURLConnection conn = (HttpURLConnection) urlObject.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        PrintWriter out = new PrintWriter(conn.getOutputStream());
+        out.print(params.toUrlParams());
+        out.close();
+
+        int responseCode = conn.getResponseCode();
+
+        String jsonString = "";
+
+        try {
+            jsonString = getStringFromInputStream(conn.getInputStream());
+        } catch (Exception e) {
+            Log.e(TAG, Lazy.Ex.getStackTrace(e));
+        } finally {
+            conn.disconnect();
+        }
+
+        return new JSONObject();
+    }
+
+    static private String getStringFromInputStream(InputStream is) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+        String line = "";
+        while ((line = br.readLine()) != null) {
+            sb.append(line);
+        }
+        return sb.toString();
+    }
+
+    private static String getStringFromUrl(String url) throws MalformedURLException, JSONException, IOException {
+        URL urlObject = new URL(url);
+        HttpURLConnection urlConn = (HttpURLConnection) urlObject.openConnection();
+
+        String jsonString = "";
+
+        try {
+            jsonString = getStringFromInputStream(urlConn.getInputStream());
+        } finally {
+            urlConn.disconnect();
+        }
+
+        return jsonString;
+    }
 }
